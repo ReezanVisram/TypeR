dcf3a2b3391036711495587f279fd4a3b909b3bb
Reezan Visram <reezan.visram@rogers.com>
2022-03-26 16:11:33 -0400

    add multiple retries to database in server

--- server/src/lib/database.ts
+++ server/src/lib/database.ts
@@ -6,16 +6,27 @@ import {
     MYSQL_DATABASE,
 } from './config';
 
+let retries = 0;
 const createDatabaseConnection = async () => {
-    await createConnection({
-        type: 'mysql',
-        host: MYSQL_HOST,
-        database: MYSQL_DATABASE,
-        username: MYSQL_USERNAME,
-        password: MYSQL_ROOT_PASSWORD,
-        entities: [`${__dirname}/../entities/*{.ts,.js}`],
-        synchronize: true,
-    });
+    if (retries >= 3) {
+        console.log('Attempted to connect to databse 3 times, disconnecting');
+        return;
+    }
+    try {
+        await createConnection({
+            type: 'mysql',
+            host: MYSQL_HOST,
+            database: MYSQL_DATABASE,
+            username: MYSQL_USERNAME,
+            password: MYSQL_ROOT_PASSWORD,
+            entities: [`${__dirname}/../entities/*{.ts,.js}`],
+            synchronize: true,
+        });
+    } catch {
+        console.log(`Unable to connect to database, retrying (${retries})`);
+        retries++;
+        setTimeout(createDatabaseConnection, 2500);
+    }
 };
 
 export default createDatabaseConnection;
